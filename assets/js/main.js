2087104569c4db2c48dee45e3dfeebe3bf77566b
Lattanaxay <Lattanaxaycaster@gmail.com>
2023-08-18 23:37:41 +0800

    add javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('navmenu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+/*=============== MENU SHOW ===============*/
 
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*=============== MENU HIDEN ===============*/
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== SHADOW HEADER ===============*/
-
 
 /*=============== EMAIL JS ===============*/
 
